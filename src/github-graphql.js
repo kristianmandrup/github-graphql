95f208c9ddc6a26387266345766e74858afabde3
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-12 07:19:46 -0430

    GithubUser class

--- src/github-graphql.js
+++ src/github-graphql.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Octokat = require('octokat');
+var GithubUser = require('./user');
 
 // require('./server');
 
@@ -10,8 +11,9 @@ var Octokat = require('octokat');
  * @example
  * let Github = require( './src/github-graphql' );
  * let github = new Github();
-*/
+ */
 module.exports = class Github  {
+
   greet() {
     return 'hello';
   }
@@ -24,24 +26,19 @@ module.exports = class Github  {
    *
    * @param credentials.username
    * @param credentials.password
-  */
+   */
   authenticate(credentials){
     this.octo = new Octokat({
       username: credentials.username,
       password: credentials.password
     });
+
+    this.authenticateUser = new GithubUser(credentials, this.octo);
   }
 
-  /**
-   * Return all authenticate user's organizations
-   *
-   * @example
-   * github.userOrgs.then( (data) => console.log(data));
-  */
-  get userOrgs(){
-    return this.octo.user.orgs.fetch();
+  get user() {
+    return this.authenticateUser;
   }
-
 
   /**
    * Return organization
@@ -50,52 +47,9 @@ module.exports = class Github  {
    * github.org('freddyucvTest').then((data) => console.log(data))
    *
    * @param name Organization's name
-  */
+   */
   org(name){
     return this.octo.orgs(name).fetch();
-  }
-
-  /**
-   * Return all repositories that are owned by the authenticated user.
-   *
-   * @example
-   * github.ownerRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-  */
-  get ownerRepos(){
-    return this.octo.user.repos.fetch({affiliation: "owner"});
-  }
-
-  /**
-   * Return all repositories that the user has been added to as a collaborator.
-   *
-   * @example
-   * github.collaboratorRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-  */
-  get collaboratorRepos(){
-    return this.octo.user.repos.fetch({affiliation: "collaborator"});
-  }
-
-  /**
-   * Return all Repositories that the user has access to through being a member
-   * of an organization. This includes every repository on every team that the
-   * user is on.
-   *
-   * @example
-   * github.organizationMemberRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-  */
-  get organizationMemberRepos(){
-    return this.octo.user.repos.fetch({affiliation: "organization_member"});
-  }
-
-
-  /**
-   * Return a list repositories that are accessible to the authenticated user
-   *
-   * @example
-   * github.userRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-  */
-  get userRepos(){
-    return this.octo.user.repos.fetch();
   }
 
 };
--- test_git_hub.js
+++ test_git_hub.js
@@ -5,9 +5,9 @@ let github = new Github();
 
 github.authenticate({username: "freddyucv", password: "leones2009"});
 
-//github.userOrgs.then((data) => {console.log(data);});
+//github.user.orgs.then((data) => {console.log(data);});
 //github.org('freddyucvTest').then((data) => console.log(data));
-//github.ownerRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-//github.collaboratorRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-//github.organizationMemberRepos.then((data) => data.forEach(elem => console.log(elem.name)));
-github.userRepos.then((data) => data.forEach(elem => console.log(elem.name)));
+//github.user.ownerRepos.then((data) => data.forEach(elem => console.log(elem.name)));
+//ithub.user.collaboratorRepos.then((data) => data.forEach(elem => console.log(elem.name)));
+//github.user.organizationMemberRepos.then((data) => data.forEach(elem => console.log(elem.name)));
+github.user.repos.then((data) => data.forEach(elem => console.log(elem.name)));
