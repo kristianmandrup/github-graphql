1e902d1e9d9123b6f7e666b07ae64a6728eca85b
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-16 22:41:43 -0430

    graphql server and schema second attemp

---
+++ src/graphql-server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import graphqlHTTP from 'graphql-koa';
+import mount from 'koa-mount';
+import schema from './graphql/schema/schema'
+
+var graphqlServer = mount('/graphql', graphqlHTTP({ schema: schema }));
+
+export default graphqlServer;
