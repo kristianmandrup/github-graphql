a089ee0c03b8ffc4af62fab6b68cb9d337c3dd13
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-16 20:58:25 -0430

    Schema first attempt

---
+++ src/graphql/schema/org.js
@@ -0,0 +1,62 @@
+import {
+  GraphQLEnumType,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql.type';
+
+import Github from '../github/github-util'
+let github = new Github();
+
+var orgType = new GraphQLObjectType({
+  name: 'organization',
+  description: 'Github organization',
+  id: {
+    type: new GraphQLNonNull(GraphQLString),
+    description: 'The github server id of the org.',
+  },
+  login: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  },
+  url: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  },
+  reposUrl: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  },
+  eventsUrl: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  },
+  avatarUrl: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  },
+  description: {
+    type: new GraphQLString(GraphQLString),
+    description: '',
+  }
+});
+
+
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    org: {
+      type: userType,
+      args: {
+        orgName: {
+          description: '',
+          type: GraphQLString
+        }
+      },
+      resolve: (root, { orgName }) => github.org(orgName),
+    }
+
+});
---
+++ src/graphql/schema/user.js
@@ -0,0 +1,153 @@
+import {
+  GraphQLEnumType,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql.type';
+
+import Github from '../github/github-util'
+let github = new Github();
+
+var userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'Github user',
+  id: {
+    type: new GraphQLNonNull(GraphQLString),
+    description: 'The github server id of the user.',
+  },
+  login: {
+    type: GraphQLString,
+    description: 'The login of the user.',
+  },
+  avatarUrl: {
+    type: GraphQLString,
+    description: 'User avatar URL',
+  },
+  htmlUrl: {
+    type: GraphQLString,
+    description: 'User html',
+  },
+  /*followersUrl: {
+    type: GraphQLString,
+    description: '',
+  },
+  subscriptionsUrl: {
+    type: GraphQLString,
+    description: '',
+  },
+  organizationsUrl: {
+    type: GraphQLString,
+    description: '',
+  },
+  reposUrl: {
+    type: GraphQLString,
+    description: '',
+  },
+  receivedEventsUrl: {
+    type: GraphQLString,
+    description: '',
+  },*/
+  type: {
+    type: GraphQLString,
+    description: '',
+  },
+  name: {
+    type: GraphQLString,
+    description: '',
+  },
+  company: {
+    type: GraphQLString,
+    description: '',
+  },
+  blog: {
+    type: GraphQLString,
+    description: '',
+  },
+  location: {
+    type: GraphQLString,
+    description: '',
+  },
+  email: {
+    type: GraphQLString,
+    description: '',
+  },
+  hireable: {
+    type: GraphQLString,
+    description: '',
+  },
+  bio: {
+    type: GraphQLString,
+    description: '',
+  },
+  publicRepos: {
+    type: GraphQLInt,
+    description: '',
+  },
+  publicGists: {
+    type: GraphQLInt,
+    description: '',
+  },
+  createdAt: {
+    type: GraphQLString,
+    description: '',
+  },
+  updatedAt: {
+    type: GraphQLString,
+    description: '',
+  },
+  privateGists: {
+    type: GraphQLInt,
+    description: '',
+  },
+  privateGists: {
+    type: GraphQLInt,
+    description: '',
+  },
+  totalPrivateRepos: {
+    type: GraphQLInt,
+    description: '',
+  },
+  ownedPrivateRepos: {
+    type: GraphQLInt,
+    description: '',
+  },
+  diskUsage: {
+    type: GraphQLInt,
+    description: '',
+  },
+  collaborators: {
+    type: GraphQLInt,
+    description: '',
+  },
+  plan: new GraphQLObjectType({
+        name: 'Plan',
+        fields: {
+          name: { type: GraphQLString },
+          space: { type: GraphQLInt },
+          collaborators: { type: GraphQLInt },
+          privateRepos: { type: GraphQLInt }
+        }
+  })
+});
+
+/*
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    user: {
+      type: userType,
+      args: {
+        userName: {
+          description: '',
+          type: GraphQLString
+        }
+      },
+      resolve: (root, { userName }) => github.authenticate(userName),
+    }
+
+});
+
+*/
