967bf1a876fac90c0781165adf1ef60d2649b7af
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-19 14:52:04 -0430

    Organization schema test

--- test/unit/graphql/graphql-server-test.js
+++ test/unit/graphql/graphql-server-test.js
@@ -1,1 +1,34 @@
-import schema from '../../../src/graphql/schema/schema';
+import request from 'supertest-as-promised';
+import server from '../../../src/server';
+import { stringify } from 'querystring';
+import agent from 'supertest-koa-agent';
+
+describe('Server: ', () => {
+
+  let app;
+
+  beforeEach(() => {
+    app = agent(server);
+  });
+
+  describe('Organization ', () => {
+    describe('Getting organization description by username ', () => {
+      let query = `query orgs_query {
+                    orgs(userName: "freddyucv") {
+                      description
+                    }
+                  }`;
+
+      var url = '/graphql?' + stringify({query: query})
+
+      console.log(url);
+
+      app.get(url).expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+
+          console.log(JSON.stringify(res.body));
+        });
+    });
+  });
+});
