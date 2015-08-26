d719d351f8a1b546528c1ddf9a9d26de136a946b
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-26 10:52:41 -0430

    Schema refactoring

--- test/unit/graphql/schema-test.js
+++ test/unit/graphql/schema-test.js
@@ -33,5 +33,31 @@ describe('Schema: ', () => {
       );
     });
 
+    it('all organizations description', (done) => {
+      let query = `query orgs_query {
+                     orgs(userName: "freddyucv") {
+                       description
+                       teams
+                     }
+                   }`;
+
+      let resultExpect = { orgs: [ { description: 'freddyucvTest' } ] };
+
+      let user = {};
+
+      //TODO: change by a fake github server
+      user.octo =new Octokat({
+        username: 'freddyucv',
+        password: 'leones2009'
+      });
+
+      graphql(schema, query, user).then((data) =>
+        {
+          done();
+          expect(data).to.deep.equal(resultExpect);
+        }
+      );
+    });
+
   });
 });
