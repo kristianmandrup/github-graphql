347700497dcf121af6f0efb3f2439d6c13dfc2f9
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-08-26 21:46:32 -0430

    Get org by name query (need refactoring)

---
+++ src/graphql/schemas/queries/type/team-type.js
@@ -0,0 +1,38 @@
+import {
+  GraphQLEnumType,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLString
+} from 'graphql';
+
+/*
+type Team {
+  id: String!
+  name: String
+  description: String
+  members: [User] // or TeamMember?
+  // ...
+}
+*/
+
+export default new GraphQLObjectType({
+  name: 'Team',
+  description: 'Organization or user teams',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The github server id of the team.',
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Organization description',
+    },
+    description: {
+      type: GraphQLString,
+      description: 'Organization description',
+    }
+  })
+});
